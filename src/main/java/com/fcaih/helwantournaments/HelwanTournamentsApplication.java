db33f6369d6439864296ce2abb392f0a2412c47a
justFadel19 <ahmedfadelaf885794@gmail.com>
2024-03-14 02:00:07 +0200

    Initialized the project

---
+++ src/main/java/com/fcaih/helwantournaments/HelwanTournamentsApplication.java
@@ -0,0 +1,13 @@
+package com.fcaih.helwantournaments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelwanTournamentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelwanTournamentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcaih/helwantournaments/HelwanTournamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcaih.helwantournaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelwanTournamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
