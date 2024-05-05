75223f7902da42193c673163acc13507190a01df
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-05-05 06:43:34 +0300

    Created the team service interface.

---
+++ backend/src/main/java/com/fcaih/helwantournaments/service/TeamService.java
@@ -0,0 +1,14 @@
+package com.fcaih.helwantournaments.service;
+
+import java.util.List;
+
+import com.fcaih.helwantournaments.model.Team;
+import com.fcaih.helwantournaments.model.User;
+
+public interface TeamService {
+    Team saveTeam(Team team);
+    Team getTeamById(Long id);
+    void deleteTeam(Long id);
+    Team updateTeamName(Long id, String name);
+    Team updateTeamPlayers(Long id, List<User> players);
+}
