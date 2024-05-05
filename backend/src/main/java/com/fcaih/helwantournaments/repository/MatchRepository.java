02f3884f7150a66b3209a5d3be07ba751525f0f1
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-05-06 00:14:49 +0300

    Added find matches by tournament Id function.

--- backend/src/main/java/com/fcaih/helwantournaments/repository/MatchRepository.java
+++ backend/src/main/java/com/fcaih/helwantournaments/repository/MatchRepository.java
@@ -1,9 +1,11 @@
 package com.fcaih.helwantournaments.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fcaih.helwantournaments.model.Match;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
-
+   List<Match> findByTournamentId(Long tournamentId);
 }
