d0adba703965427d2f0321fefbe2bde503204e9d
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-05-05 03:03:09 +0300

    Updated the user repository, and added the optional datatype to handle null errors.

--- backend/src/main/java/com/fcaih/helwantournaments/repository/UserRepository.java
+++ backend/src/main/java/com/fcaih/helwantournaments/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.fcaih.helwantournaments.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fcaih.helwantournaments.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-    User findByUsername(String username);
-    User findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
