f13941c14414e01854aec213695d05b709e5b791
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-05-05 05:10:15 +0300

    No new changes, just added a white space.

--- backend/src/main/java/com/fcaih/helwantournaments/repository/UserRepository.java
+++ backend/src/main/java/com/fcaih/helwantournaments/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fcaih.helwantournaments.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
 }
