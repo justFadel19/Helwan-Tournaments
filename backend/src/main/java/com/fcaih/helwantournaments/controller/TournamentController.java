2e6031a914306223f69ad1452b7e19de1eb41703
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-05-07 19:11:44 +0300

    Applied cross origin to connect the endpoints with the frontend.

--- backend/src/main/java/com/fcaih/helwantournaments/controller/TournamentController.java
+++ backend/src/main/java/com/fcaih/helwantournaments/controller/TournamentController.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/tournaments")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TournamentController {
 
     private TournamentService tournamentService;
